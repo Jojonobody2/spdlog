52a957a424f6f825c4eb4cf869dcf3f0b460adab
Jojonobody2 <81681680+Jojonobody2@users.noreply.github.com>
2023-07-24 22:05:59 +0200

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,38 @@
+project "spdlog"
+	kind "StaticLib"
+	language "C++"
+	staticruntime "off"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+    "src/**.cpp"
+	}
+
+  includedirs
+  {
+    "include"
+  }
+
+	filter "system:linux"
+		pic "On"
+
+		systemversion "latest"
+
+	filter "system:windows"
+		systemversion "latest"
+
+		defines 
+		{ 
+			"_CRT_SECURE_NO_WARNINGS"
+		}
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
